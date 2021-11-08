abb73a6a565bb1362c68f6bdcb0483aee9cb78ce
James Byrne <jambyr@bas.ac.uk>
2021-11-08 21:18:05 +0000

    Updated for live runs

--- icenet_train_test.sh
+++ icenet_train_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --output=/data/hpcdata/users/jambyr/icenet.north/train.%j.%N.42.out
-#SBATCH --error=/data/hpcdata/users/jambyr/icenet.north/train.%j.%N.42.err
-#SBATCH --chdir=/data/hpcdata/users/jambyr/icenet.north
+#SBATCH --output=/data/hpcdata/users/jambyr/icenet/pipeline/train.%j.%N.42.out
+#SBATCH --error=/data/hpcdata/users/jambyr/icenet/pipeline/train.%j.%N.42.err
+#SBATCH --chdir=/data/hpcdata/users/jambyr/icenet/pipeline
 #SBATCH --mail-type=begin,end,fail,requeue
 #SBATCH --mail-user=jambyr@bas.ac.uk
 #SBATCH --time=48:00:00
@@ -12,20 +12,20 @@
 #SBATCH --partition=gpu
 #SBATCH --account=gpu
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=192gb
+#SBATCH --mem=128gb
 
 
 # BAS HPC specific items are offloaded to the ensemble configuration so that there is a single point of config
 module load hpc/cuda/11.2
 . /hpcpackages/python/miniconda3/etc/profile.d/conda.sh
-conda activate /data/hpcdata/users/jambyr/miniconda3/envs/icenet2
-cd /data/hpcdata/users/jambyr/icenet.north
+conda activate /data/hpcdata/users/jambyr/miniconda3/envs/icenet
+cd /data/hpcdata/users/jambyr/icenet/pipeline
 mkdir -p results
 
 echo "START `date +%F`"
 
-python3 train.py -v ensemble_22 ensemble.test.42 42 \
-    -b 4 -e 2 -m -w 8 -qs 10 \
+icenet_train -v 90s_22 90s.test.42 42 \
+    -b 4 -e 3 -m -w 8 -qs 10 \
 2>&1 
 
 echo "FINISH `date +%F`"
