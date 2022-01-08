6b4f36a5f4ee7f5d31939585152fffbc5d38d5d8
James Byrne <jambyr@bas.ac.uk>
2022-01-08 13:21:04 +0000

    Updated for second environment run

--- ensemble/template/bashpc.sh
+++ ensemble/template/bashpc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+. /etc/bashrc
 module load hpc/cuda/11.2
 . /hpcpackages/python/miniconda3/etc/profile.d/conda.sh
 conda activate /data/hpcdata/users/jambyr/miniconda3/envs/icenet
--- install_env.sh
+++ install_env.sh
@@ -9,6 +9,8 @@ NAME=$1
 LOGNAME=${2:-${0%%.sh}.log}
 ERRNAME=${2:-${0%%.sh}.err}
 
-echo "Environment $NAME install started `date +"%F %T"`" >$LOGNAME
-conda env create -q --file environment.yml -n $NAME 2>$ERRNAME >>$LOGNAME
-echo "Environment $NAME install finished `date +"%F %T"`" >>$LOGNAME
+mkdir logs
+
+echo "Environment $NAME install started `date +"%F %T"`" >logs/$LOGNAME
+conda env create -q --file environment.yml -n $NAME 2>logs/$ERRNAME >>logs/$LOGNAME
+echo "Environment $NAME install finished `date +"%F %T"`" >>logs/$LOGNAME
