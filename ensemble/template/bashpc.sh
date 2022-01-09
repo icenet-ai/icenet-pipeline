26b388cd29a5e3076d4e9b5aee76843048d05f5c
James Byrne <jambyr@bas.ac.uk>
2022-01-09 18:56:27 +0000

    BAS specific shell update

--- ensemble/template/bashpc.sh
+++ ensemble/template/bashpc.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-. /etc/bashrc
+[ -f /etc/bashrc ] && . /etc/bashrc
+
 module load hpc/cuda/11.2
 . /hpcpackages/python/miniconda3/etc/profile.d/conda.sh
 conda activate /data/hpcdata/users/jambyr/miniconda3/envs/icenet
 
+# Don't like this but unavoidable at present
+if [ -f /data/hpcdata/users/$USER/.wandb.env ]; then
+   echo "Loading WANDB configuration specifically for BAS"
+   . /data/hpcdata/users/$USER/.wandb.env
+fi
