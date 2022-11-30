70633931490d379c4f9035b49d3c0bc5039d84a5
James Byrne <jambyr@bas.ac.uk>
2022-11-30 17:27:08 +0000

    BAS HPC preamble was wonky, fixed

--- ensemble/template/bashpc.sh
+++ ensemble/template/bashpc.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 [ -f /etc/bashrc ] && . /etc/bashrc
 
-. ENVS
-
-conda activate $ICENET_CONDA
-
 # Don't like this but unavoidable at present
 if [ -f /data/hpcdata/users/$USER/.wandb.env ]; then
    echo "Loading WANDB configuration specifically for BAS"
    . /data/hpcdata/users/$USER/.wandb.env
 fi
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/hpcpackages/jupyterhub/20200401/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/hpcpackages/jupyterhub/20200401/etc/profile.d/conda.sh" ]; then
+        . "/hpcpackages/jupyterhub/20200401/etc/profile.d/conda.sh"
+    else
+        export PATH="/hpcpackages/jupyterhub/20200401/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
