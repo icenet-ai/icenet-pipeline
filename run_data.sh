17997be534093167c0e53e993724e1b62adad599
James Byrne <jambyr@bas.ac.uk>
2023-01-17 10:56:07 +0000

    Added skip block to run data for existent loader config

--- run_data.sh
+++ run_data.sh
@@ -12,21 +12,25 @@ HEMI="${1:-$HEMI}"
 BATCH_SIZE=${2:-2}
 WORKERS=${3:-8}
 
-[ ! -z "$PROC_ARGS_ERA5" ] && icenet_process_era5 -v -l $LAG \
-    $PROC_ARGS_ERA5 \
-    -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
-    ${DATANAME}_${HEMI} $HEMI
+if [ ! -f loader.${DATANAME}_${HEMI}.json ]; then
+    [ ! -z "$PROC_ARGS_ERA5" ] && icenet_process_era5 -v -l $LAG \
+        $PROC_ARGS_ERA5 \
+        -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
+        ${DATANAME}_${HEMI} $HEMI
 
-[ ! -z "$PROC_ARGS_ORAS5" ] && icenet_process_oras5 -v -l $LAG \
-    $PROC_ARGS_ORAS5 \
-    -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
-    ${DATANAME}_${HEMI} $HEMI
+    [ ! -z "$PROC_ARGS_ORAS5" ] && icenet_process_oras5 -v -l $LAG \
+        $PROC_ARGS_ORAS5 \
+        -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
+        ${DATANAME}_${HEMI} $HEMI
 
-[ ! -z "$PROC_ARGS_SIC" ] && icenet_process_sic -v -l $LAG \
-    $PROC_ARGS_SIC \
-    -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
-    ${DATANAME}_${HEMI} $HEMI
+    [ ! -z "$PROC_ARGS_SIC" ] && icenet_process_sic -v -l $LAG \
+        $PROC_ARGS_SIC \
+        -ns $TRAIN_START -ne $TRAIN_END -vs $VAL_START -ve $VAL_END -ts $TEST_START -te $TEST_END \
+        ${DATANAME}_${HEMI} $HEMI
 
-icenet_process_metadata ${DATANAME}_${HEMI} $HEMI
+    icenet_process_metadata ${DATANAME}_${HEMI} $HEMI
+else
+    echo "Skipping preprocessing as loader.${DATANAME}_${HEMI}.json already exists..."
+fi
 
 icenet_dataset_create -v -p -ob $BATCH_SIZE -w $WORKERS -fd $FORECAST_DAYS -l $LAG ${DATANAME}_${HEMI} $HEMI
