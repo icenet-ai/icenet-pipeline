9ab638a942356242feb47461381739d4a722df80
James Byrne <jambyr@bas.ac.uk>
2023-01-17 16:52:03 +0000

    Correcting outputs

--- run_forecast_plots.sh
+++ run_forecast_plots.sh
@@ -17,13 +17,15 @@ if [ -d $OUTPUT_DIR ]; then
 fi
 mkdir -p $OUTPUT_DIR
 
+echo "Reading ${FORECAST_NAME}.csv"
+
 cat ${FORECAST_NAME}.csv | while read -r FORECAST_DATE; do
-    echo "Producing binary accuracy for $LINE";
+    echo "Producing binary accuracy for $FORECAST_DATE";
     icenet_plot_bin_accuracy -e -v \
         -o ${OUTPUT_DIR}/bin_accuracy.${FORECAST_DATE}.png \
         $HEMI $FORECAST_FILE $FORECAST_DATE >>$BINACC_LOG 2>&1
 
-    echo "Producing SIC error for $LINE";
+    echo "Producing SIC error for $FORECAST_DATE";
     icenet_plot_sic_error -v \
         -o ${OUTPUT_DIR}/sic_error.${FORECAST_DATE}.mp4 \
         $HEMI $FORECAST_FILE $FORECAST_DATE >>$SICERR_LOG 2>&1
