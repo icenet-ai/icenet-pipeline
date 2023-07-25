31ad3abb9530c0a1fae2466d5d45f8811c133a3a
James Byrne <jambyr@bas.ac.uk>
2023-07-25 15:33:25 +0100

    Fixing data usage for predictions in era5_forecast

--- run_era5_forecast.sh
+++ run_era5_forecast.sh
@@ -17,11 +17,13 @@ for HEMI in south north; do
     icenet_data_era5 -w 10 -v $DATA_ARGS_ERA5 $HEMI $DATE_RANGE 2>&1 | tee logs/fc.era5.${HEMI}.log
     icenet_data_sic -v $HEMI $DATE_RANGE 2>&1 | tee logs/fc.sic.${HEMI}.log
 
-    FORECAST_INIT=`python -c 'import xarray as xr; print(str(xr.open_dataset("data/osisaf/'$HEMI'/siconca/'${DATE_RANGE:0:4}'.nc").time.values.max())[0:10])'`
+#   ERA5 tracks behind the SIC, there was an issue causing the need to use SIC
+#    FORECAST_INIT=`python -c 'import xarray as xr; print(str(xr.open_dataset("data/osisaf/'$HEMI'/siconca/'${DATE_RANGE:0:4}'.nc").time.values.max())[0:10])'`
+    FORECAST_INIT=`python -c 'import xarray as xr; print(str(xr.open_dataset("data/era5/'$HEMI'/tas/'${DATE_RANGE:0:4}'.nc").time.values.max())[0:10])'`
 
     export FORECAST_START="$FORECAST_INIT"
     export FORECAST_END="$FORECAST_INIT"
-    ./run_prediction.sh fc.$FORECAST_INIT $PREDICTION_NETWORK $HEMI forecast $TRAIN_DATA_NAME 2>&1 | tee logs/fc.${HEMI}.log
+    ./run_prediction.sh fc.$FORECAST_INIT ${PREDICTION_NETWORK}_${HEMI} $HEMI forecast $TRAIN_DATA_NAME 2>&1 | tee logs/fc.${HEMI}.log
 
     ./produce_op_assets.sh fc.${FORECAST_INIT}_${HEMI} 2>&1 | tee logs/op_assets.${HEMI}.log
 done
