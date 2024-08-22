f2bd7d74d7ba7bfbfbd6d497ca7a061e0fb21f65
James Byrne <jambyr@bas.ac.uk>
2024-08-22 15:08:09 +0100

    Training data working for both hemispheres

--- prep_training_data.sh
+++ prep_training_data.sh
@@ -43,22 +43,21 @@ ATMOS_PROC="era5_osi"
 GROUND_TRUTH_SIC_DSC="data/${GROUND_TRUTH_SIC}/${DATASET_CONFIG_NAME}"
 ATMOS_PROC_DSC="data/${ATMOS_PROC}/${DATASET_CONFIG_NAME}"
 
-PROCESSED_DATASET="training"
+PROCESSED_DATASET="training.${HEMI}"
 LOADER_CONFIGURATION="loader.${PROCESSED_DATASET}.json"
-DATASET_NAME="tfdata_cache"
+DATASET_NAME="tfdata_${HEMI}"
 
 ## Workflow
 preprocess_loader_init -v $PROCESSED_DATASET
 
 preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET land "icenet.data.masks.osisaf:Masks"
-  # TODO: masks is not compatible with dual hemisphere in this form!
 preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET polarhole "icenet.data.masks.osisaf:Masks"
 preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET active_grid_cell "icenet.data.masks.osisaf:Masks"
 
 preprocess_missing_time -n siconca -v $OSISAF_DATASET $GROUND_TRUTH_SIC
   # TODO: didn't seemingly detect missing months? data/osi_sic/month/hemi.north/siconca.missing_days.csv
   # TODO: undoubtedly need to include the known invalid dates - added these to the osisaf downloader
-preprocess_missing_spatial -m processed.masks.json -mp land,active_grid_cell,polarhole -n siconca -v $GROUND_TRUTH_SIC_DSC
+preprocess_missing_spatial -m processed.masks.${HEMI}.json -mp land,active_grid_cell,polarhole -n siconca -v $GROUND_TRUTH_SIC_DSC
   # TODO: Interpolation failing in all cases?
   # TODO: this undoubtedly explains the stray nans present in dataset generation
 
@@ -68,16 +67,13 @@ preprocess_dataset $PROC_ARGS_SIC -v \
   $GROUND_TRUTH_SIC_DSC ${PROCESSED_DATASET}_osisaf
   # TODO: plenty of nans contained in here due to failing spatial interpolation - needs investigation
 
-# TODO: icenet_osisaf_ref for geospatial metadata -v data/osisaf/hemi.north/siconca/???.nc ref.osisaf.north.nc
-#  this needs to:
-#  - ds = xr.open_dataset("./data/osisaf/month/hemi.north/siconca/1978.nc")
-#  - ds = ds.drop_vars(["raw_ice_conc_values", "smearing_standard_error", "algorithm_standard_error"])
-#  - cube = ds.siconca.to_iris()
-#  - cube.coord('projection_x_coordinate').convert_units('meters')
-#  - cube.coord('projection_y_coordinate').convert_units('meters')
-#  - iris.save("ref.osisaf.nc")
+HEMI_SHORT="nh"
+[ $HEMI == "south" ] && HEMI_SHORT="sh"
+# TODO: we should be able to preseve data during download-toolbox processing for this, but
+#  alas this needs some investigation to achieve, so this will work for the moment
+icenet_generate_ref_osisaf -v data/masks/ice_conc_${HEMI_SHORT}_ease2-250_cdr-v2p0_200001021200.nc
 
-preprocess_regrid -v $ERA5_DATASET ref.osisaf.nc $ATMOS_PROC
+preprocess_regrid -v $ERA5_DATASET ref.osisaf.${HEMI}.nc $ATMOS_PROC
   # TODO: get the batcher back in place for multiprocessing this
   # TODO: this should regrid ALL files in the dataset, for some reason 2024.nc did not get wrapped in
 preprocess_rotate -n uas,vas -v $ATMOS_PROC_DSC ref.osisaf.nc
@@ -98,10 +94,10 @@ preprocess_add_channel -v $LOADER_CONFIGURATION $GROUND_TRUTH_SIC_DSC land_map "
 
 icenet_dataset_create -v -c -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
 
-# TODO: select a random date from the training set, plot and log so user can double check outputs
-icenet_plot_input -p -v dataset_config.${DATASET_NAME}.json 1985-04-30 ./plot/input.png
-icenet_plot_input --outputs -v dataset_config.${DATASET_NAME}.json 1985-04-30 ./plot/outputs.png
-icenet_plot_input --weights -v dataset_config.${DATASET_NAME}.json 1985-04-30 ./plot/weights.png
+FIRST_DATE=`cat ${LOADER_CONFIGURATION} | jq '.sources[.sources|keys[0]].splits.train[0]' | tr -d '"'`
+icenet_plot_input -p -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/input.${HEMI}.${FIRST_DATE}.png
+icenet_plot_input --outputs -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/outputs.${HEMI}.${FIRST_DATE}.png
+icenet_plot_input --weights -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/weights.${HEMI}.${FIRST_DATE}.png
 
 icenet_dataset_create -v -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
   # TODO: FIXME in here to override the creation of nan containing sets due to earlier issues
