9ab9a4e57827a978c595f6317cbc9803e3aa87c5
James Byrne <jambyr@bas.ac.uk>
2024-08-27 23:33:50 +0100

    Dev #53: implementing prediction and more comprehensive lifecycle, BUT with significant issues around download-toobox

--- prep_prediction_data.sh
+++ prep_prediction_data.sh
@@ -1,11 +1,114 @@
-#!/bin/bash
+#!/usr/bin/bash -l
 
-source ENVS
+set -e -o pipefail
+
+. ENVS
 
 conda activate $ICENET_CONDA
 
-set -o pipefail
-set -eu
+if [ $# -lt 2 ] || [ "$1" == "-h" ]; then
+    echo "Usage $0 <prediction_name> <hemisphere> [date_vars] [train_data_name]"
+    echo "<prediction_name> name of prediction dataset"
+    echo "<hemisphere>      hemisphere to use"
+    echo "[date_vars]       variables for defining start and end dates to forecast"
+    echo "[train_data_name] name of data used to train the model"
+    echo "Options: none"
+    exit 1
+fi
 
+# obtaining any arguments that should be passed onto run_forecast_plots.sh
+OPTIND=1
+while getopts "" opt; do
+    case "$opt" in
+    esac
+done
 
+shift $((OPTIND-1))
 
+echo "Leftovers from getopt: $@"
+
+PREDICTION_NAME="$1"
+HEMI="$2"
+DATE_VARS="${3:-$PREDICTION_NAME}"
+DATA_PROC="${4:-${TRAIN_DATA_NAME}}.${HEMI}"
+
+NAME_START="${DATE_VARS^^}_START"
+NAME_END="${DATE_VARS^^}_END"
+echo "Dates from ENVS: $NAME_START and $NAME_END"
+PREDICTION_START=${!NAME_START}
+PREDICTION_END=${!NAME_END}
+
+if [ -z $PREDICTION_START ] || [ -z $PREDICTION_END ]; then
+    echo "Prediction date args not set correctly: \"$PREDICTION_START\" to \"$PREDICTION_END\""
+    exit 1
+else
+    echo "Prediction start arg: $PREDICTION_START"
+    echo "Prediction end arg: $PREDICTION_END"
+fi
+
+PREDICTION_DATASET="prediction.${PREDICTION_NAME}.${HEMI}"
+LOADER_CONFIGURATION="loader.${PREDICTION_DATASET}.json"
+
+PRED_DATA_START=`date --date "$PREDICTION_START - $LAG ${DATA_FREQUENCY}s" +%Y-%m-%d`
+# download-toolbox integration
+(
+  # download_amsr2 $DATA_ARGS $HEMI $AMSR2_DATES $AMSR2_VAR_ARGS
+  download_osisaf $DATA_ARGS $HEMI $PRED_DATA_START $PREDICTION_END $OSISAF_VAR_ARGS
+  download_era5 $DATA_ARGS $HEMI $PRED_DATA_START $PREDICTION_END $ERA5_VAR_ARGS
+  # download_cmip --source MRI-ESM2-0 --member r1i1p1f1 $DATA_ARGS $HEMI $CMIP6_DATES $CMIP6_VAR_ARGS
+
+  # TODO: this overwrites the ./data/osisaf/dataset_config.month.hemi.north.json files, which is unacceptable - localise
+  # TODO: we inadvertently clone existing datasets which is also unacceptable for predictions - filter data accordingly
+) 2>&1 | tee logs/download.${PREDICTION_DATASET}.log
+
+DATASET_CONFIG_NAME="dataset_config.${DATA_FREQUENCY}.hemi.${HEMI}.json"
+
+# preprocess-toolbox integration
+# Persistent datasets from the source data store, wherever that is
+OSISAF_DATASET="${SOURCE_DATA_STORE}/osisaf/${DATASET_CONFIG_NAME}"
+ERA5_DATASET="${SOURCE_DATA_STORE}/era5/${DATASET_CONFIG_NAME}"
+ATMOS_PROC="era5_osi.$PREDICTION_DATASET"
+ATMOS_PROC_DSC="data/${ATMOS_PROC}/${DATASET_CONFIG_NAME}"
+
+# Create links to the central data store datasets for easier "mapping"
+[ ! -e data/osisaf ] && [ -d ${SOURCE_DATA_STORE}/osisaf ] && ln -s ${SOURCE_DATA_STORE}/osisaf ./data/osisaf
+[ ! -e data/era5 ] && [ -d ${SOURCE_DATA_STORE}/era5 ] && ln -s ${SOURCE_DATA_STORE}/era5 ./data/era5
+# TODO: AMSR
+# TODO: CMIP
+
+LOADER_CONFIGURATION="loader.${PREDICTION_DATASET}.json"
+
+preprocess_loader_init -v $PREDICTION_DATASET
+preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET land "icenet.data.masks.osisaf:Masks"
+preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET polarhole "icenet.data.masks.osisaf:Masks"
+preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET active_grid_cell "icenet.data.masks.osisaf:Masks"
+
+preprocess_dataset $PROC_ARGS_SIC -v \
+  -sn "prediction" -ss "$PREDICTION_START" -se "$PREDICTION_END" \
+  -r processed/${DATA_PROC}_osisaf/ \
+  -i "icenet.data.processors.osisaf:SICPreProcessor" \
+  $OSISAF_DATASET ${PREDICTION_DATASET}_osisaf
+
+if [ ! -f ref.osisaf.${HEMI}.nc ]; then
+  echo "Reference OSISAF for regrid should still be available, bailing for the mo"
+  exit 1
+fi
+
+preprocess_regrid -v $ERA5_DATASET ref.osisaf.${HEMI}.nc $ATMOS_PROC
+preprocess_rotate -n uas,vas -v $ATMOS_PROC_DSC ref.osisaf.${HEMI}.nc
+
+preprocess_dataset $PROC_ARGS_ERA5 -v \
+  -sn "prediction" -ss "$PREDICTION_START" -se "$PREDICTION_END" \
+  -r processed/${DATA_PROC}_era5/ \
+  -i "icenet.data.processors.cds:ERA5PreProcessor" \
+  $ATMOS_PROC_DSC ${PREDICTION_DATASET}_era5
+
+preprocess_add_processed -v $LOADER_CONFIGURATION processed.${PREDICTION_DATASET}_osisaf.json processed.${PREDICTION_DATASET}_era5.json
+preprocess_add_channel -v $LOADER_CONFIGURATION $OSISAF_DATASET sin "icenet.data.meta:SinProcessor"
+preprocess_add_channel -v $LOADER_CONFIGURATION $OSISAF_DATASET cos "icenet.data.meta:CosProcessor"
+preprocess_add_channel -v $LOADER_CONFIGURATION $OSISAF_DATASET land_map "icenet.data.masks.osisaf:Masks"
+
+icenet_dataset_create -v -c -p -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $PREDICTION_DATASET
+
+FIRST_DATE=${PLOT_DATE:-`cat ${LOADER_CONFIGURATION} | jq '.sources[.sources|keys[0]].splits.prediction[0]' | tr -d '"'`}
+icenet_plot_input -p -v dataset_config.${PREDICTION_DATASET}.json $FIRST_DATE ./plot/input.${HEMI}.${FIRST_DATE}.png
--- prep_training_data.sh
+++ prep_training_data.sh
@@ -21,7 +21,7 @@ if [ $DOWNLOAD -eq 1 ]; then
   download_osisaf $DATA_ARGS $HEMI $OSISAF_DATES $OSISAF_VAR_ARGS
   download_era5 $DATA_ARGS $HEMI $ERA5_DATES $ERA5_VAR_ARGS
   # download_cmip --source MRI-ESM2-0 --member r1i1p1f1 $DATA_ARGS $HEMI $CMIP6_DATES $CMIP6_VAR_ARGS
-fi 2>&1 | tee logs/download.log
+fi 2>&1 | tee logs/download.training.log
 
 DATASET_CONFIG_NAME="dataset_config.${DATA_FREQUENCY}.hemi.${HEMI}.json"
 
@@ -36,14 +36,14 @@ ERA5_DATASET="${SOURCE_DATA_STORE}/era5/${DATASET_CONFIG_NAME}"
 # TODO: AMSR
 # TODO: CMIP
 
-GROUND_TRUTH_SIC="osi_sic"
-ATMOS_PROC="era5_osi"
+GROUND_TRUTH_SIC="osi_sic.$TRAIN_DATA_NAME"
+ATMOS_PROC="era5_osi.$TRAIN_DATA_NAME"
 
 # Our processed dataset configurations, we localise data when regridding and reprojecting
 GROUND_TRUTH_SIC_DSC="data/${GROUND_TRUTH_SIC}/${DATASET_CONFIG_NAME}"
 ATMOS_PROC_DSC="data/${ATMOS_PROC}/${DATASET_CONFIG_NAME}"
 
-PROCESSED_DATASET="training.${HEMI}"
+PROCESSED_DATASET="${TRAIN_DATA_NAME}.${HEMI}"
 LOADER_CONFIGURATION="loader.${PROCESSED_DATASET}.json"
 DATASET_NAME="tfdata_${HEMI}"
 
@@ -55,28 +55,23 @@ preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET polarhole "icenet.d
 preprocess_add_mask -v $LOADER_CONFIGURATION $OSISAF_DATASET active_grid_cell "icenet.data.masks.osisaf:Masks"
 
 preprocess_missing_time -n siconca -v $OSISAF_DATASET $GROUND_TRUTH_SIC
-  # TODO: didn't seemingly detect missing months? data/osi_sic/month/hemi.north/siconca.missing_days.csv
-  # TODO: undoubtedly need to include the known invalid dates - added these to the osisaf downloader
 preprocess_missing_spatial -m processed.masks.${HEMI}.json -mp land,active_grid_cell,polarhole -n siconca -v $GROUND_TRUTH_SIC_DSC
-  # TODO: Interpolation failing in all cases?
-  # TODO: this undoubtedly explains the stray nans present in dataset generation
 
 preprocess_dataset $PROC_ARGS_SIC -v \
   -ps "train" -sn "train,val,test" -ss "$TRAIN_START,$VAL_START,$TEST_START" -se "$TRAIN_END,$VAL_END,$TEST_END" \
   -i "icenet.data.processors.osisaf:SICPreProcessor" \
   $GROUND_TRUTH_SIC_DSC ${PROCESSED_DATASET}_osisaf
-  # TODO: plenty of nans contained in here due to failing spatial interpolation - needs investigation
 
 HEMI_SHORT="nh"
 [ $HEMI == "south" ] && HEMI_SHORT="sh"
-# TODO: we should be able to preseve data during download-toolbox processing for this, but
+# TODO: we should be able to preserve data during download-toolbox processing for this, but
 #  alas this needs some investigation to achieve, so this will work for the moment
 icenet_generate_ref_osisaf -v data/masks/ice_conc_${HEMI_SHORT}_ease2-250_cdr-v2p0_200001021200.nc
 
 preprocess_regrid -v $ERA5_DATASET ref.osisaf.${HEMI}.nc $ATMOS_PROC
   # TODO: get the batcher back in place for multiprocessing this
   # TODO: this should regrid ALL files in the dataset, for some reason 2024.nc did not get wrapped in
-preprocess_rotate -n uas,vas -v $ATMOS_PROC_DSC ref.osisaf.nc
+preprocess_rotate -n uas,vas -v $ATMOS_PROC_DSC ref.osisaf.${HEMI}.nc
   # TODO: get the batcher back in place for multiprocessing this
 
 preprocess_dataset $PROC_ARGS_ERA5 -v \
@@ -94,10 +89,9 @@ preprocess_add_channel -v $LOADER_CONFIGURATION $GROUND_TRUTH_SIC_DSC land_map "
 
 icenet_dataset_create -v -c -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
 
-FIRST_DATE=`cat ${LOADER_CONFIGURATION} | jq '.sources[.sources|keys[0]].splits.train[0]' | tr -d '"'`
+FIRST_DATE=${PLOT_DATE:-`cat ${LOADER_CONFIGURATION} | jq '.sources[.sources|keys[0]].splits.train[0]' | tr -d '"'`}
 icenet_plot_input -p -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/input.${HEMI}.${FIRST_DATE}.png
 icenet_plot_input --outputs -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/outputs.${HEMI}.${FIRST_DATE}.png
 icenet_plot_input --weights -v dataset_config.${DATASET_NAME}.json $FIRST_DATE ./plot/weights.${HEMI}.${FIRST_DATE}.png
 
 icenet_dataset_create -v -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
-  # TODO: FIXME in here to override the creation of nan containing sets due to earlier issues
